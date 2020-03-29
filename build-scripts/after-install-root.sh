f9bdfe452791870ec22d60c342088fc450d3caa7
Nino Dafonte <nino.dafonte@gmail.com>
2020-03-29 12:09:31 +0200

    Adding clean up after deploy

--- build-scripts/after-install-root.sh
+++ build-scripts/after-install-root.sh
@@ -3,9 +3,9 @@
 # Application preparation
 #######################################################################
 (
-    rm -rf build-scripts
-    rm appspec.yml
-    rm meeting-card-vectors.pdf
-    rm meeting-card-vectors.svg
+    rm -rf /var/www/cards.dti.cat/build-scripts
+    rm /var/www/cards.dti.cat/appspec.yml
+    rm /var/www/cards.dti.cat/meeting-card-vectors.pdf
+    rm /var/www/cards.dti.cat/meeting-card-vectors.svg
     chown -R www-data: /var/www/cards.dti.cat
 )
