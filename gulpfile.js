6ec19aa3b30c8af06a719cc82c45bd6b4797ed4f
Nino Dafonte <nino.dafonte@gmail.com>
2020-04-14 14:27:00 +0200

    Remove images folder from build in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,9 @@ function clean(cb) {
 
 function mainFiles(cb) {
   return gulp.src([
-    './images',
-    'index.html',
+    'index.html'
   ]).pipe(gulp.dest('./dist'));
 }
 
-function images(cb) {
-  return gulp.src([
-    './images/**/*',
-  ]).pipe(gulp.dest('./dist/images'));
-}
 
-exports.default = gulp.series(clean, mainFiles, images);
+exports.default = gulp.series(clean, mainFiles);
